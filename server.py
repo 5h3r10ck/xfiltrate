1ab6469b5fb13cd30f092cacca536fa79888ff00
5h3r10ck <adilaheyad@gmail.com>
2020-08-09 20:47:44 +0100

    args

--- server.py
+++ server.py
@@ -1,2 +1,12 @@
 # C2 Server
+
 #!/usr/bin/env python3
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--command", help="execute command")
+parser.add_argument("-t", "--transfer", help="transfer files")
+
+args = parser.parse_args()
+
